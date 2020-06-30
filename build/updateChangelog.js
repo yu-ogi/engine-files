9bc367fe96e6c18c2dfbb01f1ac477767e4299e9
dera- <alchemy.and.fairy@gmail.com>
2020-06-30 12:42:26 +0000

    add script to update CHANGELOG

---
+++ build/updateChangelog.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const fs = require("fs");
+
+console.log("start to update changelog");
+const packageJson = require(path.join(__dirname, "..", "package.json"));
+const replacedText = `# CHANGELOG
+
+## ${packageJson["version"]}
+* @akashic/akashic-engine: ${packageJson["dependencies"]["@akashic/akashic-engine"]}
+* @akashic/akashic-pdi: ${packageJson["dependencies"]["@akashic/akashic-pdi"]}
+* @akashic/game-driver: ${packageJson["dependencies"]["@akashic/game-driver"]}
+* @akashic/pdi-browser: ${packageJson["devDependencies"]["@akashic/pdi-browser"]}
+* @akashic/playlog-client: ${packageJson["optionalDependencies"]["@akashic/playlog-client"]}`;
+const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "CHANGELOG.md")).toString();
+const nextChangeLog = currentChangeLog.replace("# CHANGELOG", replacedText);
+fs.writeFileSync(path.join(__dirname, "..", "CHANGELOG.md"), nextChangeLog);
+console.log("end to update changelog");
