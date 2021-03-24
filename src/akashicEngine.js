7c15dd0d68450249fa5145580397105ddb843799
xnv <xnv@users.noreply.github.com>
2021-03-24 21:26:43 +0900

    fix broken refs

--- src/akashicEngine.js
+++ src/akashicEngine.js
@@ -7,9 +7,9 @@ var akashicEngine = require("@akashic/akashic-engine");
 // これらの値は、TS 上では interface (型だけの存在) に見える一方 JS レイヤーでは値が存在する、という状況になる。
 // 元々 engine-files の利用者のレイヤーでは JS としてしか扱っておらず、
 // また TS として利用している場合は値を参照することがないので問題はない。
-akashicEngine.AudioAsset = require("@akashic/pdi-common-impl/lib/AudioAsset");
-akashicEngine.AudioPlayer = require("@akashic/pdi-common-impl/lib/AudioPlayer");
-akashicEngine.ScriptAsset = require("@akashic/pdi-common-impl/lib/ScriptAsset");
-akashicEngine.TextAsset = require("@akashic/pdi-common-impl/lib/TextAsset");
+akashicEngine.AudioAsset = require("@akashic/pdi-common-impl/lib/AudioAsset").AudioAsset;
+akashicEngine.AudioPlayer = require("@akashic/pdi-common-impl/lib/AudioPlayer").AudioPlayer;
+akashicEngine.ScriptAsset = require("@akashic/pdi-common-impl/lib/ScriptAsset").ScriptAsset;
+akashicEngine.TextAsset = require("@akashic/pdi-common-impl/lib/TextAsset").TextAsset;
 
 module.exports = akashicEngine;
