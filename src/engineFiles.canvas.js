cbc6bf8996ee8ee00e734ed6ccf9e48e7562118a
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2021-04-16 13:29:54 +0900

    fix: fix import path

--- src/engineFiles.canvas.js
+++ src/engineFiles.canvas.js
@@ -1,5 +1,5 @@
 module.exports = {
 	gameDriver: require("@akashic/game-driver"),
 	akashicEngine: require("./akashicEngine"),
-	pdiBrowser: typeof window !== "undefined" ? require("@akashic/pdi-browser") : null
+	pdiBrowser: typeof window !== "undefined" ? require("@akashic/pdi-browser/lib/canvas") : null
 };
