e77413743c55607f6997f68bcc5fec35f8a601f6
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2022-05-19 16:47:48 +0900

    feat: migrate to v2-main

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import * as akashicEngine from "@akashic/akashic-engine";
 import * as gameDriver from "@akashic/game-driver";
-import * as pdi from "@akashic/pdi-types";
+import * as pdi from "@akashic/akashic-pdi";
 
 export {
 	akashicEngine,
